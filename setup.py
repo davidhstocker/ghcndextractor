063b67c5b953c41e8c640aa89e50005188ebaa60
David Stocker <david.stocker@sap.com>
2016-05-01 09:35:30 +0200

    added setup.py and setup.cfg for pypi release

---
+++ setup.py
@@ -0,0 +1,19 @@
+'''
+Created on May 1, 2016
+
+@author: David Stocker
+'''
+
+from distutils.core import setup
+setup(
+  name = 'ghcndextractor',
+  packages = ['ghcndextractor'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A simple extractor for the GHCN-Daily dataset .dly files into CSV or dict format.',
+  author = 'David Stocker',
+  author_email = 'mrdave991@gmail.com',
+  url = 'https://github.com/davidhstocker/ghcndextractor', # use the URL to the github repo
+  download_url = 'https://github.com/davidhstocker/ghcndextractor/tarball/0.11', 
+  keywords = ['ghcnd', 'ghcnddaily', 'climatedata'], # arbitrary keywords
+  classifiers = [],
+)
