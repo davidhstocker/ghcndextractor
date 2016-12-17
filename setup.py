58bee1efd18e731e982ea8d9c630843ad9baa702
David Stocker <david.stocker@sap.com>
2016-12-17 09:15:31 +0100

    Updated setup.py for 0.2 release

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ from distutils.core import setup
 setup(
   name = 'ghcndextractor',
   packages = ['ghcndextractor'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.2',
   description = 'A simple extractor for the GHCN-Daily dataset .dly files into CSV or dict format.',
   author = 'David Stocker',
   author_email = 'mrdave991@gmail.com',
   url = 'https://github.com/davidhstocker/ghcndextractor', # use the URL to the github repo
-  download_url = 'https://github.com/davidhstocker/ghcndextractor/tarball/0.11', 
+  download_url = 'https://github.com/davidhstocker/ghcndextractor/tarball/0.2', 
   keywords = ['ghcnd', 'ghcnddaily', 'climatedata'], # arbitrary keywords
   classifiers = [],
 )
